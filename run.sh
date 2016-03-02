8ac20ae8ed4369be6712b08178dc2c6b002ed7a4
Your Name <you@example.com>
2016-03-02 08:11:06 +0800

    direct run from aliyun hangzhou docker registry

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-docker run -dt -v $(pwd)/conf:/app/conf wx-robot
+docker run -dt -v $(pwd)/conf:/app/conf registry.aliyuncs.com/river/weixin-robot-docker
 log=conf/weixin-robot.log
 mv $log $log.$(date +%s)
 touch $log
